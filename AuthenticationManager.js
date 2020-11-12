1bd7057153ac541e7806cc6f1fcf808121fbde7d
Leonardo Robol <leo@robol.it>
2020-11-12 21:46:26 +0100

    Use more recent sharelatex.

--- AuthenticationManager.js
+++ AuthenticationManager.js
@@ -1,12 +1,12 @@
 const Settings = require('settings-sharelatex')
-const {User} = require('../../models/User')
-const {db, ObjectId} = require('../../infrastructure/mongojs')
+const { User } = require('../../models/User')
+const { db, ObjectId } = require('../../infrastructure/mongojs')
 const bcrypt = require('bcrypt')
 const EmailHelper = require('../Helpers/EmailHelper')
 const V1Handler = require('../V1/V1Handler')
 const {
-    InvalidEmailError,
-    InvalidPasswordError
+  InvalidEmailError,
+  InvalidPasswordError
 } = require('./AuthenticationErrors')
 const util = require('util')
 const ldap = require('ldapjs')
@@ -14,76 +14,26 @@ const ldap = require('ldapjs')
 const BCRYPT_ROUNDS = Settings.security.bcryptRounds || 12
 const BCRYPT_MINOR_VERSION = Settings.security.bcryptMinorVersion || 'a'
 
-const _checkWriteResult = function (result, callback) {
-    // for MongoDB
-    if (result && result.nModified === 1) {
-        callback(null, true)
-    } else {
-        callback(null, false)
-    }
+const _checkWriteResult = function(result, callback) {
+  // for MongoDB
+  if (result && result.nModified === 1) {
+    callback(null, true)
+  } else {
+    callback(null, false)
+  }
 }
 
 const AuthenticationManager = {
-    authenticate(query, password, callback) {
-        // Using Mongoose for legacy reasons here. The returned User instance
-        // gets serialized into the session and there may be subtle differences
-        // between the user returned by Mongoose vs mongojs (such as default values)
-        User.findOne(query, (error, user) => {
-            AuthenticationManager.authUserObj(error, user, query, password, callback)
-        })
-    },
-    
-    //login with any passwd
-    login(user, password, callback) {
-        AuthenticationManager.checkRounds(
-            user,
-            user.hashedPassword,
-            password,
-            function (err) {
-                if (err) {
-                    return callback(err)
-                }
-                callback(null, user)
-            }
-        )
-    },
-
-    createIfNotExistAndLogin(query, adminMail, user, callback) {
-        if (query.email != adminMail & (!user || !user.hashedPassword)) {
-            //create random pass for local userdb, does not get checked for ldap users during login
-            let pass = require("crypto").randomBytes(32).toString("hex")
-            const userRegHand = require('../User/UserRegistrationHandler.js')
-            userRegHand.registerNewUser({
-                    email: query.email,
-                    password: pass
-                },
-                function (error, user) {
-                    if (error) {
-                        callback(error)
-                    }
-                    user.admin = false
-		    user.emails[0].confirmedAt = Date.now()
-	            user.save()
-                    console.log("user %s added to local library", query.email)
-                    User.findOne(query, (error, user) => {
-                            if (error) {
-                                return callback(error)
-                            }
-                            if (user && user.hashedPassword) {
-                                AuthenticationManager.login(user, "randomPass", callback)
-                            }
-                        }
-                    )
-
-
-                })
-            //return callback(null, null)
-        } else {
-            AuthenticationManager.login(user, "randomPass", callback)
-        }
-    },
-
-    authUserObj(error, user, query, password, callback) {
+authenticate(query, password, callback) {
+    // Using Mongoose for legacy reasons here. The returned User instance
+    // gets serialized into the session and there may be subtle differences
+    // between the user returned by Mongoose vs mongodb (such as default values)
+    User.findOne(query, (error, user) => {
+      AuthenticationManager.authUserObj(error, user, query, password, callback);
+    });
+  },
+  
+  authUserObj(error, user, query, password, callback) {
         //non ldap / local admin user
         const adminMail = process.env.ADMIN_MAIL
         const domain = process.env.DOMAIN
@@ -119,169 +69,59 @@ const AuthenticationManager = {
         //check if user is in ldap
         AuthenticationManager.ldapAuth(query, password, AuthenticationManager.createIfNotExistAndLogin, callback, adminMail, user)
     },
-
-    validateEmail(email) {
-        const parsed = EmailHelper.parseEmail(email)
-        if (!parsed) {
-            return new InvalidEmailError({message: 'email not valid'})
-        }
-        return null
-    },
-
-    // validates a password based on a similar set of rules to `complexPassword.js` on the frontend
-    // note that `passfield.js` enforces more rules than this, but these are the most commonly set.
-    // returns null on success, or an error string.
-    validatePassword(password) {
-        if (password == null) {
-            return new InvalidPasswordError({
-                message: 'password not set',
-                info: {code: 'not_set'}
-            })
-        }
-
-        let allowAnyChars, min, max
-        if (Settings.passwordStrengthOptions) {
-            allowAnyChars = Settings.passwordStrengthOptions.allowAnyChars === true
-            if (Settings.passwordStrengthOptions.length) {
-                min = Settings.passwordStrengthOptions.length.min
-                max = Settings.passwordStrengthOptions.length.max
-            }
-        }
-        allowAnyChars = !!allowAnyChars
-        min = min || 6
-        max = max || 72
-
-        // we don't support passwords > 72 characters in length, because bcrypt truncates them
-        if (max > 72) {
-            max = 72
-        }
-
-        if (password.length < min) {
-            return new InvalidPasswordError({
-                message: 'password is too short',
-                info: {code: 'too_short'}
-            })
-        }
-        if (password.length > max) {
-            return new InvalidPasswordError({
-                message: 'password is too long',
-                info: {code: 'too_long'}
-            })
-        }
-        if (
-            !allowAnyChars &&
-            !AuthenticationManager._passwordCharactersAreValid(password)
-        ) {
-            return new InvalidPasswordError({
-                message: 'password contains an invalid character',
-                info: {code: 'invalid_character'}
-            })
-        }
-        return null
-    },
-
-    setUserPassword(userId, password, callback) {
-        AuthenticationManager.setUserPasswordInV2(userId, password, callback)
-    },
-
-    checkRounds(user, hashedPassword, password, callback) {
-        // Temporarily disable this function, TODO: re-enable this
-        if (Settings.security.disableBcryptRoundsUpgrades) {
-            return callback()
-        }
-        // check current number of rounds and rehash if necessary
-        const currentRounds = bcrypt.getRounds(hashedPassword)
-        if (currentRounds < BCRYPT_ROUNDS) {
-            AuthenticationManager.setUserPassword(user._id, password, callback)
-        } else {
-            callback()
-        }
-    },
-
-    hashPassword(password, callback) {
-        bcrypt.genSalt(BCRYPT_ROUNDS, BCRYPT_MINOR_VERSION, function (error, salt) {
-            if (error) {
-                return callback(error)
-            }
-            bcrypt.hash(password, salt, callback)
-        })
-    },
-
-    setUserPasswordInV2(userId, password, callback) {
-        const validationError = this.validatePassword(password)
-        if (validationError) {
-            return callback(validationError)
-        }
-        this.hashPassword(password, function (error, hash) {
-            if (error) {
-                return callback(error)
-            }
-            db.users.update(
-                {
-                    _id: ObjectId(userId.toString())
-                },
-                {
-                    $set: {
-                        hashedPassword: hash
-                    },
-                    $unset: {
-                        password: true
-                    }
-                },
-                function (updateError, result) {
-                    if (updateError) {
-                        return callback(updateError)
-                    }
-                    _checkWriteResult(result, callback)
+    
+    //login with any passwd
+    login(user, password, callback) {
+        AuthenticationManager.checkRounds(
+            user,
+            user.hashedPassword,
+            password,
+            function (err) {
+                if (err) {
+                    return callback(err)
                 }
-            )
-        })
-    },
-
-    setUserPasswordInV1(v1UserId, password, callback) {
-        const validationError = this.validatePassword(password)
-        if (validationError) {
-            return callback(validationError.message)
-        }
-
-        V1Handler.doPasswordReset(v1UserId, password, function (error, reset) {
-            if (error) {
-                return callback(error)
+                callback(null, user)
             }
-            callback(error, reset)
-        })
+        )
     },
-
-    _passwordCharactersAreValid(password) {
-        let digits, letters, lettersUp, symbols
-        if (
-            Settings.passwordStrengthOptions &&
-            Settings.passwordStrengthOptions.chars
-        ) {
-            digits = Settings.passwordStrengthOptions.chars.digits
-            letters = Settings.passwordStrengthOptions.chars.letters
-            lettersUp = Settings.passwordStrengthOptions.chars.letters_up
-            symbols = Settings.passwordStrengthOptions.chars.symbols
+    
+    createIfNotExistAndLogin(query, adminMail, user, callback) {
+        if (query.email != adminMail & (!user || !user.hashedPassword)) {
+            //create random pass for local userdb, does not get checked for ldap users during login
+            let pass = require("crypto").randomBytes(32).toString("hex")
+            const userRegHand = require('../User/UserRegistrationHandler.js')
+            userRegHand.registerNewUser({
+                email: query.email,
+                password: pass
+            },
+            function (error, user) {
+                if (error) {
+                    callback(error)
+                }
+                
+                const update = {
+	               admin: false,
+		       'emails.0.confirmedAt': Date.now()
+                };
+             
+                console.log("user %s added to local library", query.email)
+                User.findOneAndUpdate(query, update, (error, user) => {
+                    if (error) {
+                        console.log(error);
+                        return callback(error)
+                    }
+                    if (user && user.hashedPassword) {
+                        AuthenticationManager.login(user, "randomPass", callback)
+                    }
+               })
+           })
+            //return callback(null, null)
+        } else {
+            AuthenticationManager.login(user, "randomPass", callback)
         }
-        digits = digits || '1234567890'
-        letters = letters || 'abcdefghijklmnopqrstuvwxyz'
-        lettersUp = lettersUp || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        symbols = symbols || '@#$%^&*()-_=+[]{};:<>/?!£€.,'
-
-        for (let charIndex = 0; charIndex <= password.length - 1; charIndex++) {
-            if (
-                digits.indexOf(password[charIndex]) === -1 &&
-                letters.indexOf(password[charIndex]) === -1 &&
-                lettersUp.indexOf(password[charIndex]) === -1 &&
-                symbols.indexOf(password[charIndex]) === -1
-            ) {
-                return false
-            }
-        }
-        return true
     },
-
-    ldapAuth(query, passwd, onSuccess, callback, adminMail, userObj) {
+  
+  ldapAuth(query, passwd, onSuccess, callback, adminMail, userObj) {
         
         const tlsOpts = { 
             checkServerIdentity: function(serverName, cert) {
@@ -326,13 +166,174 @@ const AuthenticationManager = {
                 });
             }  
         });
+    },    
+
+  validateEmail(email) {
+    const parsed = EmailHelper.parseEmail(email)
+    if (!parsed) {
+      return new InvalidEmailError({ message: 'email not valid' })
     }
-    
+    return null
+  },
+
+  // validates a password based on a similar set of rules to `complexPassword.js` on the frontend
+  // note that `passfield.js` enforces more rules than this, but these are the most commonly set.
+  // returns null on success, or an error string.
+  validatePassword(password) {
+    if (password == null) {
+      return new InvalidPasswordError({
+        message: 'password not set',
+        info: { code: 'not_set' }
+      })
+    }
+
+    let allowAnyChars, min, max
+    if (Settings.passwordStrengthOptions) {
+      allowAnyChars = Settings.passwordStrengthOptions.allowAnyChars === true
+      if (Settings.passwordStrengthOptions.length) {
+        min = Settings.passwordStrengthOptions.length.min
+        max = Settings.passwordStrengthOptions.length.max
+      }
+    }
+    allowAnyChars = !!allowAnyChars
+    min = min || 6
+    max = max || 72
+
+    // we don't support passwords > 72 characters in length, because bcrypt truncates them
+    if (max > 72) {
+      max = 72
+    }
+
+    if (password.length < min) {
+      return new InvalidPasswordError({
+        message: 'password is too short',
+        info: { code: 'too_short' }
+      })
+    }
+    if (password.length > max) {
+      return new InvalidPasswordError({
+        message: 'password is too long',
+        info: { code: 'too_long' }
+      })
+    }
+    if (
+      !allowAnyChars &&
+      !AuthenticationManager._passwordCharactersAreValid(password)
+    ) {
+      return new InvalidPasswordError({
+        message: 'password contains an invalid character',
+        info: { code: 'invalid_character' }
+      })
+    }
+    return null
+  },
+
+  setUserPassword(userId, password, callback) {
+    AuthenticationManager.setUserPasswordInV2(userId, password, callback)
+  },
+
+  checkRounds(user, hashedPassword, password, callback) {
+    // Temporarily disable this function, TODO: re-enable this
+    if (Settings.security.disableBcryptRoundsUpgrades) {
+      return callback()
+    }
+    // check current number of rounds and rehash if necessary
+    const currentRounds = bcrypt.getRounds(hashedPassword)
+    if (currentRounds < BCRYPT_ROUNDS) {
+      AuthenticationManager.setUserPassword(user._id, password, callback)
+    } else {
+      callback()
+    }
+  },
+
+  hashPassword(password, callback) {
+    bcrypt.genSalt(BCRYPT_ROUNDS, BCRYPT_MINOR_VERSION, function(error, salt) {
+      if (error) {
+        return callback(error)
+      }
+      bcrypt.hash(password, salt, callback)
+    })
+  },
+
+  setUserPasswordInV2(userId, password, callback) {
+    const validationError = this.validatePassword(password)
+    if (validationError) {
+      return callback(validationError)
+    }
+    this.hashPassword(password, function(error, hash) {
+      if (error) {
+        return callback(error)
+      }
+      db.users.update(
+        {
+          _id: ObjectId(userId.toString())
+        },
+        {
+          $set: {
+            hashedPassword: hash
+          },
+          $unset: {
+            password: true
+          }
+        },
+        function(updateError, result) {
+          if (updateError) {
+            return callback(updateError)
+          }
+          _checkWriteResult(result, callback)
+        }
+      )
+    })
+  },
+
+  setUserPasswordInV1(v1UserId, password, callback) {
+    const validationError = this.validatePassword(password)
+    if (validationError) {
+      return callback(validationError.message)
+    }
+
+    V1Handler.doPasswordReset(v1UserId, password, function(error, reset) {
+      if (error) {
+        return callback(error)
+      }
+      callback(error, reset)
+    })
+  },
+
+  _passwordCharactersAreValid(password) {
+    let digits, letters, lettersUp, symbols
+    if (
+      Settings.passwordStrengthOptions &&
+      Settings.passwordStrengthOptions.chars
+    ) {
+      digits = Settings.passwordStrengthOptions.chars.digits
+      letters = Settings.passwordStrengthOptions.chars.letters
+      lettersUp = Settings.passwordStrengthOptions.chars.letters_up
+      symbols = Settings.passwordStrengthOptions.chars.symbols
+    }
+    digits = digits || '1234567890'
+    letters = letters || 'abcdefghijklmnopqrstuvwxyz'
+    lettersUp = lettersUp || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    symbols = symbols || '@#$%^&*()-_=+[]{};:<>/?!£€.,'
+
+    for (let charIndex = 0; charIndex <= password.length - 1; charIndex++) {
+      if (
+        digits.indexOf(password[charIndex]) === -1 &&
+        letters.indexOf(password[charIndex]) === -1 &&
+        lettersUp.indexOf(password[charIndex]) === -1 &&
+        symbols.indexOf(password[charIndex]) === -1
+      ) {
+        return false
+      }
+    }
+    return true
+  }
 }
 
 AuthenticationManager.promises = {
-    authenticate: util.promisify(AuthenticationManager.authenticate),
-    hashPassword: util.promisify(AuthenticationManager.hashPassword)
+  authenticate: util.promisify(AuthenticationManager.authenticate),
+  hashPassword: util.promisify(AuthenticationManager.hashPassword),
+  setUserPassword: util.promisify(AuthenticationManager.setUserPassword)
 }
 
 module.exports = AuthenticationManager
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM sharelatex/sharelatex:2.0.1
+FROM sharelatex/sharelatex:latest
 LABEL maintainer="github.com/worksasintended"
 RUN cd /var/www/sharelatex/web/ && npm install ldapjs
 #overwrite  AuthenticationManager.js
